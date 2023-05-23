59c04fb3d98b4c5a441ba20ec4cb6a6d7fec56b2
SiHyun-Lee113 <tlgus0113@gmail.com>
2023-05-23 18:04:58 +0900

    ImageSlider

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:library_flutter/myLibrary/Button/ButtonCollection.dart';
 import 'package:library_flutter/myLibrary/Dialog/DialogCollection.dart';
 import 'package:library_flutter/myLibrary/TestWebView.dart';
 import 'package:library_flutter/myLibrary/TextField/TextCollection.dart';
+import 'package:library_flutter/myLibrary/UiCollection/ImageSlider.dart';
 
 void main() => runApp(Main());
 
@@ -70,9 +71,17 @@ class _LibraryCollectionPageState extends State<_LibraryCollectionPage> {
               ),
               ElevatedButton(
                   onPressed: () {
-
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) =>
+                            MyImageSlider(
+                              imageList: [
+                                "assets/images/test1.png",
+                                "assets/images/test2.png",
+                                "assets/images/test3.png",],
+                            )));
                   },
-                  child: Text('')
+                  child: Text('슬라이드 이미지')
               ),
               ElevatedButton(
                   onPressed: () {
